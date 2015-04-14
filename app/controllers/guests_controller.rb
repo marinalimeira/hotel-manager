ac81def913f988f8f8644d32af7970e0f4362b37
Marina Limeira <marinaflessa@gmail.com>
2015-04-14 14:06:34 -0300

    error on guests controller

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -9,14 +9,43 @@ class GuestsController < ApplicationController
         @guest = room.guests.create(guest_params)
     
         if @guest.save
-            redirect_to guest_path(@guest)
+            redirect_to @guest
         else
             render 'new'
         end
     end
     
+     def show
+       @guest = Guest.find(params[:id]) 
+    end
+    
+     def index
+        @guests = Guest.all
+    end
+    
+    def edit
+        @guest = Guest.find(params[:id]) 
+    end
+    
+    def update
+        @guest = Guest.find(params[:id]) 
+        
+        if @guest.update(guest_params)
+            redirect_to @guest
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @guest = Guest.find(params[:id])
+        @guest.destroy
+ 
+        redirect_to guests_path
+    end
+    
     private
-    def guest_params
-      params.require(:guest).permit(:room_id, :name, :days)
-    end
+        def guest_params
+            params.require(:guest).permit(:room_id, :name, :days)
+        end
 end
